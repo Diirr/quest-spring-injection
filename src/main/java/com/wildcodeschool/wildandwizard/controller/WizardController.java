2b34e67d38e10805e54a4dbb7ba004263fd547a3
Malte Knezevic <m.knezevic@telekom.de>
2020-01-14 15:00:11 +0100

    task finished

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -1,6 +1,8 @@
 package com.wildcodeschool.wildandwizard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import com.wildcodeschool.wildandwizard.entity.Wizard;
+import com.wildcodeschool.wildandwizard.repository.WizardDao;
 import com.wildcodeschool.wildandwizard.repository.WizardRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WizardController {
 
-    private WizardRepository repository = new WizardRepository();
+    @Autowired	
+    private WizardDao repository; //= new WizardRepository();
 
     @GetMapping("/wizards")
     public String getAll(Model model) {
